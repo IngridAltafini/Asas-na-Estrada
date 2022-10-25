db058901cf8cf871686e068961d407aefd48f89e
Ingrid Altafini <ingridaltafini225@gmail.com>
2022-10-25 13:44:19 -0300

    ajustes no singUp

--- frontend/src/pages/SignUp/index.js
+++ frontend/src/pages/SignUp/index.js
@@ -11,7 +11,15 @@ import { FcGoogle } from 'react-icons/fc';
 
 import { environment } from '../../shared/environment';
 
-import { Container, Background, Content, Box } from './styles';
+import {
+  Container,
+  Background,
+  Content,
+  BorderForm,
+  Form,
+  Box,
+} from './styles';
+
 import { Link } from 'react-router-dom';
 
 export const SignUp = () => {
@@ -35,48 +43,56 @@ export const SignUp = () => {
   return (
     <Container>
       <Background>
-        <h1>De moto você não aprecia a paisagem,</h1>
+        <h1>De moto você não</h1>
+        <p>aprecia a paisagem,</p>
         <h2>Você faz parte dela!</h2>
         <img src={background} alt="imagem complementar" />
       </Background>
 
       <Content>
-        <form>
-          <h1>Cadastro:</h1>
-          <h2>Crie sua conta</h2>
+        <BorderForm>
+          <Form>
+            <h1>Cadastro:</h1>
+            <h2>Crie sua conta</h2>
 
-          <input type="text" placeholder="Nome" />
-          <input type="text" placeholder="Telefone com DDD" />
-          <input type="text" placeholder="E-mail" />
-          <input type="password" placeholder="Senha" />
-          <input type="passwprd" placeholder="Confirme sua senha" />
-          <Box>
-            <ReCAPTCHA
-              sitekey={environment.RECAPTCHA_KEY}
-              onChange={() => {
-                console.log('ok');
-              }}
-            />
-          </Box>
-          <button type="button">Continuar</button>
+            <input type="text" placeholder="Nome" />
+            <input type="text" placeholder="Telefone com DDD" />
+            <input type="text" placeholder="E-mail" />
+            <input type="password" placeholder="Senha" />
+            <input type="passwprd" placeholder="Confirme sua senha" />
 
-          <h3>ou</h3>
+            <Box>
+              <ReCAPTCHA
+                sitekey={environment.RECAPTCHA_KEY}
+                onChange={() => {
+                  console.log('ok');
+                }}
+              />
+            </Box>
 
-          <button className="btn2" type="button" onClick={handleFacebookSignIn}>
-            <RiFacebookCircleFill />
-            Continuar com o Facebook
-          </button>
+            <button type="button">Continuar</button>
 
-          <button className="btn3" type="button" onClick={handleGoogleSignIn}>
-            <FcGoogle />
-            Continuar com o Google
-          </button>
-        </form>
+            <p>ou</p>
 
-        <h4>
-          Já possui uma conta em Asas na estrada?
-          <Link to="/sign-in">Logar</Link>
-        </h4>
+            <button
+              className="btn2"
+              type="button"
+              onClick={handleFacebookSignIn}
+            >
+              <RiFacebookCircleFill />
+              Continuar com o Facebook
+            </button>
+            <button className="btn3" type="button" onClick={handleGoogleSignIn}>
+              <FcGoogle />
+              Continuar com o Google
+            </button>
+
+            <strong>
+              Já possui uma conta em Asas na estrada?
+              <Link to="/sign-in">Logar</Link>
+            </strong>
+          </Form>
+        </BorderForm>
       </Content>
     </Container>
   );
--- frontend/src/pages/SignUp/styles.js
+++ frontend/src/pages/SignUp/styles.js
@@ -7,165 +7,33 @@ export const Container = styled.div`
   align-items: stretch;
 `;
 
-export const Content = styled.div`
- padding-left: 70px;
- padding-right: 70px;
-
-  position: static;
-
-  align-items: center;
-
-  background-color: ${({ theme }) => theme.background_light};
-
-  border-radius: 40px;
-  border: 7px solid;
-  border-color: ${({ theme }) => theme.secundary};
-
-  margin: 45px;
-  max-width: 100%;
-
-  form {
-
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-  
-    text-align: center;
-
-    h1 {
-      margin-top: 20px;
-      color: ${({ theme }) => theme.primary};
-      margin-bottom: 10px;
-    }
-
-    h2 {
-      margin-bottom: 25px;
-      color: ${({ theme }) => theme.text};
-    }
-
-    input {
-      font-size: 16px;
-      padding: 0.5em 0;
-      margin-bottom: 25px;
-      background-color: ${({ theme }) => theme.background_light};
-      border-bottom: 1px solid ${({ theme }) => theme.secundary_light};
-     
-
-      &::placeholder {
-        color: ${({ theme }) => theme.secundary};
-      }
-    }
-
-    button {
-      display: flex;
-      align-items: center;
-      justify-content: center;
-    
-
-
-      max-width: 100%;
-      width: 50%;
-
-      font-size: 17px;
-      font-weight: 700;
-      background-color: ${({ theme }) => theme.secundary};
-      color: ${({ theme }) => theme.background_light};
-      
-      padding: 10px;
-      margin-left: 25%;
-      margin-top: 25px;
-      margin-bottom: 10px;
-      border-radius: 30px;
-
-      transition: transform 100ms linear;
-
-      &:hover {
-        transform: scale(1.05);
-      }
-    }
-  }
-
-  h3 {
-      color: ${({ theme }) => theme.primary};
-      width: 100%;
-      font-size: 24px;
-      font-weight: 500;
-    }
-   
-
-    .btn2 {
-     
-      width: 100%;
-      margin-left: 0px;
-      margin-top: 5px;
-      color: ${({ theme }) => theme.background_light};
-      background-color: ${({ theme }) => theme.facebook};
-
-      transition: transform 100ms linear;
-
-      &:hover {
-        transform: scale(1.05);
-      }
-      svg {
-        display: flex;
-        align-items: center;
-        margin-right: 8px;
-      }
-    }
-
-    .btn3 {
-     
-      width: 100%;
-      border: 2px solid ${({ theme }) => theme.background};
-      margin-left: 0px;
-      margin-top: 5px;
-      color: ${({ theme }) => theme.text};
-      background-color: ${({ theme }) => theme.background_light};
-
-      transition: transform 100ms linear;
-
-      &:hover {
-        transform: scale(1.05);
-      }
-
-      svg {
-        display: flex;
-        align-items: center;
-        margin-right: 8px;
-      }
-    }
-  }
-
-  > h4 {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    color: ${({ theme }) => theme.text};
-
-    > a {
-      text-decoration: none;
-      color: ${({ theme }) => theme.primary};
-
-      transition: transform 100ms linear;
-
-      &:hover {
-        transform: scale(1.05);
-      }
-    }
-  
-`;
-
 export const Background = styled.div`
+  flex: 1;
+  background: no-repeat;
+  background-size: cover;
+
   > h1 {
     display: flex;
     flex-direction: center;
     text-align: center;
 
-    font-size: 37px;
+    font-size: 40px;
     color: ${({ theme }) => theme.text};
 
-    margin: 80px 30%;
-    width: 350px;
+    margin-top: 80px;
+    margin-left: 32%;
+  }
+
+  > p {
+    display: flex;
+    flex-direction: center;
+    text-align: center;
+
+    font-size: 40px;
+    color: ${({ theme }) => theme.text};
+
+    margin-left: 30.5%;
+    font-weight: 700;
   }
 
   > h2 {
@@ -176,20 +44,159 @@ export const Background = styled.div`
     font-size: 50px;
     color: ${({ theme }) => theme.primary};
 
-    margin-top: -60px;
-    margin-left: 25%;
+    margin-left: 26%;
   }
 
-  > img {
-    margin-top: 40px;
-    margin-left: 85px;
-    width: 90%;
+  img {
+    width: 750px;
+    margin-top: 50px;
+    margin-left: 50px;
+
     display: flex;
     align-items: center;
   }
 `;
 
+export const Content = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  width: 100%;
+  max-width: 750px;
+`;
+
+export const BorderForm = styled.div`
+  padding: 20px 50px;
+
+  background-color: ${({ theme }) => theme.background_light};
+
+  border-radius: 40px;
+  border: 7px solid;
+  border-color: ${({ theme }) => theme.secundary};
+`;
+
+export const Form = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 350px;
+  text-align: center;
+
+  h1 {
+    color: ${({ theme }) => theme.primary};
+    margin-bottom: 10px;
+  }
+
+  h2 {
+    margin-bottom: 25px;
+    color: ${({ theme }) => theme.text};
+  }
+
+  input {
+    font-size: 16px;
+    padding: 0.5em 0;
+    margin-bottom: 25px;
+    background-color: ${({ theme }) => theme.background_light};
+    border-bottom: 1px solid ${({ theme }) => theme.secundary_light};
+    width: 100%;
+
+    &::placeholder {
+      color: ${({ theme }) => theme.secundary};
+    }
+  }
+
+  button {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    font-size: 17px;
+    font-weight: 700;
+    background-color: ${({ theme }) => theme.secundary};
+    color: ${({ theme }) => theme.background_light};
+
+    padding: 10px 25px;
+    margin-top: 25px;
+    margin-bottom: 10px;
+    border-radius: 30px;
+
+    transition: transform 100ms linear;
+
+    &:hover {
+      transform: scale(1.05);
+    }
+  }
+
+  > p {
+    text-align: center;
+    color: ${({ theme }) => theme.primary};
+    font-size: 24px;
+    font-weight: 500;
+  }
+
+  .btn2 {
+    padding: 10px 28px;
+    margin-left: 0px;
+    margin-top: 10px;
+    color: ${({ theme }) => theme.background_light};
+    background-color: ${({ theme }) => theme.facebook};
+
+    transition: transform 100ms linear;
+
+    &:hover {
+      transform: scale(1.05);
+    }
+    svg {
+      display: flex;
+      align-items: center;
+      margin-right: 8px;
+    }
+  }
+
+  .btn3 {
+    padding: 10px 38px;
+    border: 2px solid ${({ theme }) => theme.background};
+    margin-left: 0px;
+    margin-top: 2px;
+    color: ${({ theme }) => theme.text};
+    background-color: ${({ theme }) => theme.background_light};
+
+    transition: transform 100ms linear;
+
+    &:hover {
+      transform: scale(1.05);
+    }
+
+    svg {
+      display: flex;
+      align-items: center;
+      margin-right: 8px;
+    }
+  }
+
+  > strong {
+    display: flex;
+    flex-direction: column;
+    text-align: center;
+
+    color: ${({ theme }) => theme.text};
+
+    a {
+      display: block;
+      text-align: center;
+      color: ${({ theme }) => theme.primary};
+      text-decoration: none;
+
+      transition: transform 100ms linear;
+
+      &:hover {
+        transform: scale(1.05);
+      }
+    }
+  }
+`;
+
 export const Box = styled.div`
   display: flex;
-  justify-content: center;
+  margin-left: 25px;
 `;
